8c7f3c2e7d1e92acc94c4d233bef57ba91b3d7d2
Abolfazl-Bahrami <Abalfazlbahrami1385@gmail.com>
2025-08-21 13:05:49 +0330

    add Nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/shop";
+import Nav from "./nav";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
