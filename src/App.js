ec03501bfadd6116715090e07bc5f38e8193c219
Abolfazl-Bahrami <Abalfazlbahrami1385@gmail.com>
2025-08-21 19:53:06 +0330

    Update App.js
    change folder home & nav & shop
    add contextprovider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Shop from "./pages/shop";
-import Nav from "./nav";
+import Home from "./pages/home/Home";
+import Shop from "./pages/shop/shop";
+import Nav from "./navbar/nav";
 import "bootstrap/dist/css/bootstrap.min.css"
+import { ShopContextProvider } from "./context/ShopContext";
 
 function App() {
   return (
     <div className="App">
+      <ShopContextProvider>
       <Router>
         <Nav/>
         <Routes>
@@ -15,6 +17,7 @@ function App() {
           <Route path="/shop" element={<Shop />} />
         </Routes>
       </Router>
+      </ShopContextProvider>
     </div>
   );
 }
