2f3076ce906d5cca9d0e90e6ea000c6769bfefc2
Abolfazl-Bahrami <Abalfazlbahrami1385@gmail.com>
2025-08-21 19:54:41 +0330

    products data

---
+++ src/data/products.js
@@ -0,0 +1,47 @@
+import mobile from "../images/mobile.png"
+import laptop from "../images/laptop.jpg"
+import bag from "../images/bag.jpg"
+import shoes from "../images/shoes.jpg"
+import sunglasses from "../images/sunglasses.jpg"
+import camera from "../images/camera.jpg"
+
+const PRODUCTS = [
+    {
+        id : 1,
+        productName : "mobile",
+        price : 1000 ,
+        productImage : mobile
+    },
+    {
+        id : 2,
+        productName : "laptop",
+        price : 4000 ,
+        productImage : laptop
+    },
+    {
+        id : 3,
+        productName : "bag",
+        price : 100 ,
+        productImage : bag
+    },
+    {
+        id : 4,
+        productName : "sunglasses",
+        price : 30 ,
+        productImage : sunglasses
+    },
+    {
+        id : 5,
+        productName : "shoes",
+        price : 90 ,
+        productImage : shoes
+    },
+    {
+        id : 6,
+        productName : "camera",
+        price : 300 ,
+        productImage : camera
+    }
+]
+
+export default PRODUCTS;
