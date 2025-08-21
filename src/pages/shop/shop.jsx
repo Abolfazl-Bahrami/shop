3755d9fa8be43fddebc0c0d3c62debca8c461c72
Abolfazl-Bahrami <Abalfazlbahrami1385@gmail.com>
2025-08-21 20:17:54 +0330

    Update shop.jsx
    add product to shop page when chose it and delete it if count = 0

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+import { ShopContext } from "../../context/ShopContext";
+import PRODUCTS from "../../data/products";
+import Product from "../home/product";
+
 const Shop = ()=>{
+    const {cartItem} = useContext(ShopContext)
     return(
         <div>
             <h1>Shop</h1>
+            {PRODUCTS.map((p)=>{
+                if(cartItem.some((i)=>i.id === p.id && i.count > 0))
+                return <Product data={p}/>
+            })}
         </div>
     )
 }
